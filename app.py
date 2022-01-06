5e079258eea2f2dee03d6cf20468583acc5e08f4
Murillo Stein <murillo.stein8@gmail.com>
2022-01-06 15:57:39 -0300

    update libs, if condition and st_video

--- app.py
+++ app.py
@@ -35,39 +35,44 @@ frame_size = (frame_width, frame_height)
 output_video = cv2.VideoWriter('st_test_00.mp4', cv2.VideoWriter_fourcc('M','J','P','G'), 15, frame_size)
 
 i = 0
-while True:
-    print(i)
-    i += 1
-    conectado, frame = vid_capture.read()
-    #cv2_imshow(frame)
-    if not conectado:
-        break
-    
-    resultados = reader.readtext(frame,paragraph=False,rotation_info=[0,0,0],min_size=110)
-    
 
-    for (bbox, text, prob) in resultados:
+st_video = st.empty()
 
-        print("{:.4f}: {}".format(prob, text))
-
-        # coordenadas da bounding box do OCR
-        (tl, tr, br, bl) = bbox
-        tl = (int(tl[0]), int(tl[1]))
-        tr = (int(tr[0]), int(tr[1]))
-        br = (int(br[0]), int(br[1]))
-        bl = (int(bl[0]), int(bl[1]))
-
-        # cleanup the txt and draw the box surrounding the text along
-        # with the OCR'd text itself
-        #text = cleanup_text(text)
-        cv2.rectangle(frame, tl, br, (0, 255, 0), 2)
-        cv2.putText(frame, text, (tl[0], tl[1] - 10),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-        # 
-        # cv2_imshow(frame)
+if video_file is not None:
         
-    #df_previsoes = df_previsoes.append(df_texts)
+    while True:
+        print(i)
+        i += 1
+        conectado, frame = vid_capture.read()
+        #cv2_imshow(frame)
+        if not conectado:
+            break
+        
+        resultados = reader.readtext(frame,paragraph=False,rotation_info=[0,0,0],min_size=110)
+        
 
-    output_video.write(frame)
+        for (bbox, text, prob) in resultados:
 
-st.video(output_video)
+            print("{:.4f}: {}".format(prob, text))
+
+            # coordenadas da bounding box do OCR
+            (tl, tr, br, bl) = bbox
+            tl = (int(tl[0]), int(tl[1]))
+            tr = (int(tr[0]), int(tr[1]))
+            br = (int(br[0]), int(br[1]))
+            bl = (int(bl[0]), int(bl[1]))
+
+            # cleanup the txt and draw the box surrounding the text along
+            # with the OCR'd text itself
+            #text = cleanup_text(text)
+            cv2.rectangle(frame, tl, br, (0, 255, 0), 2)
+            cv2.putText(frame, text, (tl[0], tl[1] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+            # 
+            # cv2_imshow(frame)
+            
+        #df_previsoes = df_previsoes.append(df_texts)
+
+        output_video.write(frame)
+
+    st_video.video(output_video)
