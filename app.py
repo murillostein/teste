1363027c80a424e22bb0c6d73a4f712a12479bc7
Murillo Stein <murillo.stein8@gmail.com>
2022-01-07 15:04:33 -0300

    tetes

--- app.py
+++ app.py
@@ -64,38 +64,40 @@ if video_file is not None:
         if not conectado:
             break
         st.write("nao breakou")
-        if i ==1:
+        if i == 1:
             # image = Image.open(frame)
             resultados = reader.readtext(frame,paragraph=False,rotation_info=[0,0,0])
             
             st.write("deu boa com em ler resultados")
             
-            for (bbox, text, prob) in resultados:
+                if resultados != None:
+                for (bbox, text, prob) in resultados:
 
-                print("{:.4f}: {}".format(prob, text))
+                    print("{:.4f}: {}".format(prob, text))
 
-                # coordenadas da bounding box do OCR
-                (tl, tr, br, bl) = bbox
-                tl = (int(tl[0]), int(tl[1]))
-                tr = (int(tr[0]), int(tr[1]))
-                br = (int(br[0]), int(br[1]))
-                bl = (int(bl[0]), int(bl[1]))
+                    # coordenadas da bounding box do OCR
+                    (tl, tr, br, bl) = bbox
+                    tl = (int(tl[0]), int(tl[1]))
+                    tr = (int(tr[0]), int(tr[1]))
+                    br = (int(br[0]), int(br[1]))
+                    bl = (int(bl[0]), int(bl[1]))
 
-                # cleanup the txt and draw the box surrounding the text along
-                # with the OCR'd text itself
-                #text = cleanup_text(text)
-                cv2.rectangle(frame, tl, br, (0, 255, 0), 2)
-                st.write('fez retangulo')
-                cv2.putText(frame, text, (tl[0], tl[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-                # 
-                
-                st.write('fez texto')
-                # cv2_imshow(frame)
-                
-            #df_previsoes = df_previsoes.append(df_texts)
+                    # cleanup the txt and draw the box surrounding the text along
+                    # with the OCR'd text itself
+                    #text = cleanup_text(text)
+                    cv2.rectangle(frame, tl, br, (0, 255, 0), 2)
+                    st.write('fez retangulo')
+                    cv2.putText(frame, text, (tl[0], tl[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+                    # 
+                    
+                    st.write('fez texto')
+                    # cv2_imshow(frame)
+                    
+                #df_previsoes = df_previsoes.append(df_texts)
 
-                st_empty.image(frame)
+                    st_empty.image(frame)
+                    break
 
         #output_video.write(frame)
 
