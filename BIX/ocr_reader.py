9378899c9929b612c3b90af727503571269901d0
Murillo Stein <murillo.stein8@gmail.com>
2022-01-10 13:41:46 -0300

    formatado

--- BIX/ocr_reader.py
+++ BIX/ocr_reader.py
@@ -12,105 +12,85 @@ def ocr_reader():
     
     st.title('Leitura de caracteres')
     st.subheader('Faça leitura de caracteres através de imagens ou vídeos!')
+
+    # adiciona caixa de seleção à Sidebox
     formato = st.sidebar.selectbox("Escolha o formato", ['Imagem','Vídeo'])
 
+    # carrega modelo ocr
     reader = easyocr.Reader(['en'], gpu=True)
 
     if formato == 'Vídeo':
             
         video_file = st.file_uploader("Anexe um vídeo",type = ['mp4'])
 
-
-        #if st.button('Gerar vídeo'):
-
-
-
-        # configurando video de saida
-        # output_video = cv2.VideoWriter('st_test_00.mp4', cv2.VideoWriter_fourcc('M','J','P','G'), 15, frame_size)
-
         i = 0
-
-        st_empty = st.empty()
-
-
         if video_file is not None:
             
+            # manipulação para deixar em formato legível para opencv
             tfile = tempfile.NamedTemporaryFile(delete=False)
             tfile.write(video_file.read())
 
-            # st.write("Deu boa com tfile")
             vid_capture = cv2.VideoCapture(tfile.name)
 
-            #video_bytes = video_file.read()
-
-            #st.video(video_bytes)
-
-            frame_width = int(vid_capture.get(3))
-            frame_height = int(vid_capture.get(4))
-            frame_size = (frame_width, frame_height)
             with st.spinner("Vídeo em análise"):
                 while True:
                     print(i)
-                    
                     i += 1
 
                     conectado, frame = vid_capture.read()
-
-                    # st.write("deu boa com vid_capture.read")
-
-                    # input_image = Image.open(frame)
-                    
-                    frame_cp = frame.copy()
-
-                    #cv2_imshow(frame)
-                    
+                                        
                     if not conectado:
                         break
-                    # st.write("nao breakou")
-                    # image = Image.open(frame)
+
+                    frame_cp = frame.copy()
+
+                    # aplica modelo ao frame
                     resultados = reader.readtext(frame_cp,paragraph=False,rotation_info=[0,0,0])
-                    
-                    # st.write("deu boa com em ler resultados")
-                    
                     if resultados != []:
-                        #st.write("input frame")
-                        st.image(frame)
-                        result_text = [] #empty list for results
-                        for (bbox, text, prob) in resultados:
+                    #st.write("input frame")
+                    #st.image(frame)
+                    for (bbox, text, prob) in resultados:
 
-                            print("{:.4f}: {}".format(prob, text))
+                        print("{:.4f}: {}".format(prob, text))
+                        #st.write(text)
 
-                            # coordenadas da bounding box do OCR
-                            (tl, tr, br, bl) = bbox
-                            tl = (int(tl[0]), int(tl[1]))
-                            tr = (int(tr[0]), int(tr[1]))
-                            br = (int(br[0]), int(br[1]))
-                            bl = (int(bl[0]), int(bl[1]))
+                        # coordenadas da bounding box do OCR
+                        (tl, tr, br, bl) = bbox
+                        tl = (int(tl[0]), int(tl[1]))
+                        tr = (int(tr[0]), int(tr[1]))
+                        br = (int(br[0]), int(br[1]))
+                        bl = (int(bl[0]), int(bl[1]))
 
-                            # cleanup the txt and draw the box surrounding the text along
-                            # with the OCR'd text itself
-                            #text = cleanup_text(text)
-                            cv2.rectangle(frame_cp, tl, br, (0, 255, 0), 2)
-                            #st.write('fez retangulo')
-                            cv2.putText(frame_cp, text, (tl[0], tl[1] - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-                            # 
-                            
-                            #st.write('fez texto')
-                            # cv2_imshow(frame)
-                            
-
-
-                            # for text in resultados:
-                            result_text.append(text)
-
-                        st.write(result_text)
-                            
-                        #df_previsoes = df_previsoes.append(df_texts)
+                        # cleanup the txt and draw the box surrounding the text along
+                        # with the OCR'd text itself
+                        #text = cleanup_text(text)
+                        cv2.rectangle(frame_cp, tl, br, (0, 255, 0), 2)
+                        #st.write('fez retangulo')
+                        cv2.putText(frame_cp, text, (tl[0], tl[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+                        # 
                         
-                        break
+                        #st.write('fez texto')
+                        # cv2_imshow(frame)
+
+                        #df_previsoes = pd.DataFrame()
+                        #df_previsoes[f'{i}'] = [text]
+                        #st.dataframe(df_previsoes)
+
+                        list.append(str(text))
+
+                    
+                    #break
+                    df_previsoes = pd.DataFrame()
+                    df_previsoes[f'frame: {i}'] = list
+                    st.dataframe(df_previsoes)
+
+                    st.image(frame_cp)
+                    #st.write(text)
+                    #print("{:.4f}: {}".format(text))
             #st.write("output frame")
-            st_empty.image(frame_cp)
+            st.image(frame_cp)
+            
 
     else:
             
