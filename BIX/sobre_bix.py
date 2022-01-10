82c80019abc9cd52115c2216583c1f25ac431fcb
Murillo Stein <murillo.stein8@gmail.com>
2022-01-10 10:52:07 -0300

    formatado

--- BIX/sobre_bix.py
+++ BIX/sobre_bix.py
@@ -8,20 +8,20 @@ def sobre():
     with col1:
         st.write("")
     with col2:
-        st.title("Quem somo nós")
+        st.title("Quem somos nós")
     with col3:
         st.write("")             
 
-    image = Image.open('images/sobre_nos.png')
-    st.image(image, use_column_width=True)
+    #image = Image.open('images/sobre_nos.png')
+    #st.image(image, use_column_width=True)
     st.write('Somos uma consultoria de dados, business intelligence e data science com foco em negócios e na conquista de resultados. Utilizamos as principais ferramentas e tecnologias do mercado no desenvolvimento de projetos que ajudem os nossos clientes a extrair o máximo de valor de seus dados e se transformar digitalmente. Dessa forma, acreditamos que se otimiza a gestão de processos e pessoas, o que facilita e melhora a vida de todos.')
     st.write('A BIX Tecnologia foi fundada na cidade de Florianópolis em 2014 pelo engenheiro Felipe Santos Eberhardt, que percebeu a necessidade do mercado de começar a usar dados para a criação de estratégias mais eficientes e tomada de decisões assertivas. Sem investidores ou aceleradoras, começou a conquistar clientes, aumentar a equipe e desenvolver diversos projetos de inteligência de negócios.')
     st.write('De lá para cá, o crescimento da BIX Tecnologia foi sólido, graças a uma equipe com perfil analítico e formação nas principais áreas da tecnologia, conquistando clientes em todo o Brasil, desenvolvendo projetos nos mais diversos setores das organizações e com um completo e vasto portfólio que engloba as várias verticais da economia.')
 
     st.header('')
     st.subheader('Entre em contato conosco')
-    image = Image.open('images/contato.png')
-    st.image(image, use_column_width=True)
+    #image = Image.open('images/contato.png')
+    #st.image(image, use_column_width=True)
 
 
     
